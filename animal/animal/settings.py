716c06c54e2d90979b481886e03c19e1c5c1dbb1
Vlad Popovici <mrvladpopovici@gmail.com>
2021-01-16 07:50:38 +0200

    Update settings.py
    Removed secret key

--- animal/animal/settings.py
+++ animal/animal/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '89p$m=3-mh#%$=ivvqjjt)5-%jl+6e&cx4pv5q@qjn)qhub3#v'
+SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
